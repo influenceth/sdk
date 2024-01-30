03be4f57ebc79c0dc334cce1070b32174ef9dd58
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-01-30 14:41:24 -0500

    rename order utils, add fee_scale

--- src/lib/order.js
+++ src/lib/order.js
@@ -10,16 +10,25 @@ const STATUSES = {
   CANCELLED: 3
 };
 
+const FEE_SCALE = 10000;
+
 // Calculates the required deposit for a limit buy order
-const requiredDeposit = (value, makerFee, makerBonus = 1, enforceBonus = 1) => {
-  const makerFees = Math.floor(adjustedFee(makerFee, makerBonus, enforceBonus) * value / 10000);
+const getBuyOrderDeposit = (value, makerFee, makerBonus = 1, enforceBonus = 1) => {
+  const makerFees = Math.floor(adjustedFee(makerFee, makerBonus, enforceBonus) * value / FEE_SCALE);
   return value + makerFees;
 };
 
-// Calculates the required withdrawals to player and exchange for filling a limit buy order (market sell)
-const requiredWithdrawals = (value, makerFee, takerFee, takerBonus = 1, enforceBonus = 1) => {
-  const makerFees = Math.floor((value * makerFee) / 10000); // not adjusted as it is cached at order creation
-  const takerFees = Math.floor(adjustedFee(takerFee, takerBonus, enforceBonus) * value / 10000);
+/**
+ * Calculates the required withdrawals to player and exchange for filling a limit buy order (market sell)
+ * @param {*} value is amount * unit price for the fillAmount, in microSway
+ * @param {*} makerFee is unscaled makerFee (i.e. 67 for 0.67%)
+ * @param {*} takerFee is unscaled takerFee (i.e. 67 for 0.67%)
+ * @param {*} takerBonus is feeReductionBonus of crew placing fill order
+ * @param {*} enforceBonus is feeEnforcementBonus of crew controlling exchange
+ */
+const getFillBuyOrderWithdrawals = (value, makerFee, takerFee, takerBonus = 1, enforceBonus = 1) => {
+  const makerFees = Math.floor((value * makerFee) / FEE_SCALE); // not adjusted as it is cached at order creation
+  const takerFees = Math.floor(adjustedFee(takerFee, takerBonus, enforceBonus) * value / FEE_SCALE);
 
   return {
     toExchange: makerFees + takerFees,
@@ -27,10 +36,17 @@ const requiredWithdrawals = (value, makerFee, takerFee, takerBonus = 1, enforceB
   }
 };
 
-// Calculates the required payments to player and exchange for filling a limit sell order (market buy)
-const requiredPayments = (value, makerFee, takerFee, takerBonus = 1, enforceBonus = 1) => {
-  const makerFees = Math.floor((value * makerFee) / 10000); // not adjusted as it is cached at order creation
-  const takerFees = Math.floor(adjustedFee(takerFee, takerBonus, enforceBonus) * value / 10000);
+/**
+ * Calculates the required payments to player and exchange for filling a limit sell order (market buy)
+ * @param {*} value is amount * unit price for the fillAmount, in microSway
+ * @param {*} makerFee is unscaled makerFee (i.e. 67 for 0.67%)
+ * @param {*} takerFee is unscaled takerFee (i.e. 67 for 0.67%)
+ * @param {*} takerBonus is feeReductionBonus of crew placing fill order
+ * @param {*} enforceBonus is feeEnforcementBonus of crew controlling exchange
+ */
+const getFillSellOrderPayments = (value, makerFee, takerFee, takerBonus = 1, enforceBonus = 1) => {
+  const makerFees = Math.floor((value * makerFee) / FEE_SCALE); // not adjusted as it is cached at order creation
+  const takerFees = Math.floor(adjustedFee(takerFee, takerBonus, enforceBonus) * value / FEE_SCALE);
 
   return {
     toExchange: makerFees + takerFees,
@@ -53,12 +69,13 @@ const netEffFeeBonus = (bonus, enforceBonus = 1) => {
 }
 
 export default {
+  FEE_SCALE,
   IDS,
   STATUSES,
 
-  requiredDeposit,
-  requiredWithdrawals,
-  requiredPayments,
+  getBuyOrderDeposit,
+  getFillBuyOrderWithdrawals,
+  getFillSellOrderPayments,
   adjustedFee,
   netEffFeeBonus
 };
--- test/lib/order.spec.js
+++ test/lib/order.spec.js
@@ -3,32 +3,32 @@ import order from '../../src/lib/order.js';
 
 describe('Order library', function () {
   it('should get the correct withdrawals for filling a limit buy', function () {
-    let withdrawals = order.requiredWithdrawals(1000000 * 1000, 67, 200, 1, 1);
+    let withdrawals = order.getFillBuyOrderWithdrawals(1000000 * 1000, 67, 200, 1, 1);
     expect(withdrawals.toExchange).to.equal(26700000);
     expect(withdrawals.toPlayer).to.equal(980000000);
 
-    withdrawals = order.requiredWithdrawals(1000000 * 1000, 100, 200, 1.5, 1);
+    withdrawals = order.getFillBuyOrderWithdrawals(1000000 * 1000, 100, 200, 1.5, 1);
     expect(withdrawals.toExchange).to.equal(23300000);
     expect(withdrawals.toPlayer).to.equal(986700000);
 
-    withdrawals = order.requiredWithdrawals(10000000 * 500, 667, 0, 1, 1);
+    withdrawals = order.getFillBuyOrderWithdrawals(10000000 * 500, 667, 0, 1, 1);
     expect(withdrawals.toExchange).to.equal(333500000);
     expect(withdrawals.toPlayer).to.equal(5000000000);
   });
 
   it('should get the correct payments for filling a limit sell', function () {
-    let payments = order.requiredPayments(1000000 * 1000, 67, 200, 1, 1);
+    let payments = order.getFillSellOrderPayments(1000000 * 1000, 67, 200, 1, 1);
     expect(payments.toExchange).to.equal(26700000);
     expect(payments.toPlayer).to.equal(993300000);
   });
 
   it('should fill an order with multiple partial fills', function () {
-    const total = order.requiredDeposit(1000 * 1234, 100, 1, 1);
+    const total = order.getBuyOrderDeposit(1000 * 1234, 100, 1, 1);
 
     const orders = [
-      order.requiredWithdrawals(333 * 1234, 100, 200, 1.2, 1.3),
-      order.requiredWithdrawals(333 * 1234, 100, 200, 1.5, 0.75),
-      order.requiredWithdrawals(334 * 1234, 100, 200, 0.8, 1.1)
+      order.getFillBuyOrderWithdrawals(333 * 1234, 100, 200, 1.2, 1.3),
+      order.getFillBuyOrderWithdrawals(333 * 1234, 100, 200, 1.5, 0.75),
+      order.getFillBuyOrderWithdrawals(334 * 1234, 100, 200, 0.8, 1.1)
     ];
 
     const orderTotals = orders.reduce((acc, order) => acc += (order.toPlayer + order.toExchange), 0);
