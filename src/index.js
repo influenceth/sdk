a26cabda5083170cf54181f5113e2344b834ad34
Elerium-115 <elerium115+github@gmail.com>
2024-02-06 02:56:03 +0200

    Add util to generate production chains JSON

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ import AdalianOrbit from './utils/AdalianOrbit.js';
 import Address from './utils/address.js';
 import Fixed from './utils/fixed.js';
 import Merkle from './utils/MerkleTree.js';
+import ProductionJSON from './utils/ProductionJSON.js';
 import Simplex from './utils/simplex.js';
 import Time from './utils/Time.js';
 
@@ -37,7 +38,15 @@ import ethereumContracts from './contracts/ethereum_abis.json' assert { type: 'j
 import starknetContracts from './contracts/starknet_abis.json' assert { type: 'json' };
 
 // Utility libs
-export { AdalianOrbit, Address, Fixed, Merkle, Simplex, Time };
+export {
+  AdalianOrbit,
+  Address,
+  Fixed,
+  Merkle,
+  ProductionJSON,
+  Simplex,
+  Time
+};
 
 // Game asset libs
 export {
