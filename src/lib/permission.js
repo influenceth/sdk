606fb95d0b8d293e873c948af0fb8e57e435d6ec
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-02-08 13:52:00 -0500

    permission utils

--- src/lib/permission.js
+++ src/lib/permission.js
@@ -1,6 +1,6 @@
 import Building from './building';
 import Entity from './entity';
-import inventory from './inventory';
+import Inventory from './inventory';
 
 const IDS = {
   LOT_USE: 1,
@@ -18,7 +18,7 @@ const IDS = {
   ASSEMBLE_SHIP: 13
 };
 
-const MAX_POLICY_DURATION = 12;
+const MAX_POLICY_DURATION = 31536000; // 1 yr in IRL seconds
 
 const TYPES = {
   [IDS.LOT_USE]: {
@@ -31,11 +31,11 @@ const TYPES = {
   },
   [IDS.ADD_PRODUCTS]: {
     name: 'Add Products',
-    isApplicable: (entity) => !!(entity.Inventory || entity.Inventories)?.find((i) => i.status === inventory.STATUSES.AVAILABLE)
+    isApplicable: (entity) => !!(entity.Inventory || entity.Inventories || [])?.find((i) => i.status === Inventory.STATUSES.AVAILABLE)
   },
   [IDS.REMOVE_PRODUCTS]: {
     name: 'Remove Products',
-    isApplicable: (entity) => !!(entity.Inventory || entity.Inventories)?.find((i) => i.status === inventory.STATUSES.AVAILABLE)
+    isApplicable: (entity) => !!(entity.Inventory || entity.Inventories || [])?.find((i) => i.status === Inventory.STATUSES.AVAILABLE)
   },
   [IDS.STATION_CREW]: {
     name: 'Station Crews',
@@ -75,86 +75,107 @@ const TYPES = {
   }
 };
 
-const POLICY_TYPES = [
-  {
-    key: 'private',
+const POLICY_IDS = {
+  PRIVATE: 1,
+  PUBLIC: 2,
+  PREPAID: 3,
+  CONTRACT: 4
+};
+
+const POLICY_TYPES = {
+  [POLICY_IDS.PRIVATE]: {
     name: 'Private',
     description: `The permission is retained by the controlling crew.`,
-    color: '#7e2b2a',
-    policyKey: 'ContractPolicies',
-    agreementKey: 'ContractAgreements',
-    additionSystem: 'AssignContractPolicy',
-    removalSystem: 'RemoveContractPolicy'
+    policyKey: null,
+    agreementKey: null,
+    additionSystem: null,
+    removalSystem: null,
   },
-  {
-    key: 'contract',
-    name: 'Custom Contract',
-    nameShort: 'Custom',
-    description: `The permission is granted through terms specified in the Custom Contract.`,
-    color: '#363d65',
-    policyKey: 'ContractPolicies',
-    agreementKey: 'ContractAgreements',
-    additionSystem: 'AssignContractPolicy',
-    removalSystem: 'RemoveContractPolicy'
+  [POLICY_IDS.PUBLIC]: {
+    name: 'Public',
+    description: `The permission is granted to any visiting crew.`,
+    policyKey: 'PublicPolicies',
+    agreementKey: null,
+    additionSystem: 'AssignPublicPolicy',
+    removalSystem: 'RemovePublicPolicy'
   },
-  {
-    key: 'prepaid',
+  [POLICY_IDS.PREPAID]: {
     name: 'Prepaid Lease',
     nameShort: 'Prepaid',
     description: `The permission may be leased for up to 12 months for a pre-paid sum.`,
-    color: '#185c5c',
     policyKey: 'PrepaidPolicies',
     agreementKey: 'PrepaidAgreements',
     additionSystem: 'AssignPrepaidPolicy',
     removalSystem: 'RemovePrepaidPolicy'
   },
-  {
-    key: 'public',
-    name: 'Public',
-    description: `The permission is granted to any visiting crew.`,
-    color: '#336342',
-    policyKey: 'PublicPolicies',
-    additionSystem: 'AssignPublicPolicy',
-    removalSystem: 'RemovePublicPolicy'
+  [POLICY_IDS.CONTRACT]: {
+    name: 'Custom Contract',
+    nameShort: 'Custom',
+    description: `The permission is granted through terms specified in the Custom Contract.`,
+    policyKey: 'ContractPolicies',
+    agreementKey: 'ContractAgreements',
+    additionSystem: 'AssignContractPolicy',
+    removalSystem: 'RemoveContractPolicy'
   },
-];
+};
 
-const getPolicyDetails = (entity) => {
-
-  // get the applicable policies for this entity, default policytype to private for each
-  const policies = Object.keys(TYPES)
+// get the applicable policies, agreements, and allowlists for this entity
+const getPolicyDetails = (entity, crewId = null) => {
+  return Object.keys(TYPES)
     .filter((id) => TYPES[id].isApplicable(entity))
-    .reduce((acc, permId) => ({
-      ...acc,
-      [permId]: {
-        policyType: 'private',
+    .reduce((acc, strPermId) => {
+      const permId = Number(strPermId);
+
+      // default perm policy to private
+      const permPolicy = {
+        policyType: POLICY_IDS.PRIVATE,
         policyDetails: {},
-        allowlist: [],
+        agreements: [],
+      };
+
+      // if there is one, find and apply an active policy for the perm (and related agreements)
+      Object.keys(POLICY_TYPES).forEach((key) => {
+        const { policyKey, agreementKey } = POLICY_TYPES[key];
+        if (policyKey) {
+          const activePolicy = (entity[policyKey] || []).find((p) => p.permission === permId);
+          if (activePolicy) {
+            const { permission, ...policyDetails } = activePolicy;
+            permPolicy.policyType = Number(key);
+            permPolicy.policyDetails = policyDetails;
+          }
+
+          // agreement could be from previous policy, so need to attach agreements from any policy type (even if not active)
+          if (agreementKey) {
+            permPolicy.agreements.push(...(entity[agreementKey] || []).filter((a) => a.permission === permId));
+          }
+        }
+      });
+
+      // attach whitelist for the perm
+      permPolicy.allowlist = (entity.WhitelistAgreements || []).filter((a) => a.permission === permId).map((a) => a.permitted);
+
+      // attach crew agreement status (if crew provided)
+      permPolicy.crewStatus = '';
+      if (crewId) {
+        if (entity.Control?.controller?.id === crewId) permPolicy.crewStatus = 'controller';
+        else if (permPolicy.policyType === POLICY_IDS.PUBLIC) permPolicy.crewStatus = 'granted';
+        else if (permPolicy.allowlist.find((c) => c.id === crewId)) permPolicy.crewStatus = 'granted';
+        else if (permPolicy.agreements?.find((a) => a.permitted?.id === crewId)) permPolicy.crewStatus = 'granted';
+        else if (POLICY_TYPES[permPolicy.policyType]?.agreementKey) permPolicy.crewStatus = 'available';
+        else permPolicy.crewStatus = 'restricted';
       }
-    }), {});
 
-  // find the active policy type for each (and related agreements)
-  POLICY_TYPES.forEach(({ key, policyKey, agreementKey }) => {
-    (entity[policyKey] || []).forEach(({ permission, ...policyDetails }) => {
-      policies[permission].policyType = key;
-      policies[permission].policyDetails = policyDetails;
-      if (agreementKey) {
-        policies[permission].agreements = (entity[agreementKey] || []).filter((a) => a.permission === permission);
-      }
-    });
-  });
-
-  // attach allowlist
-  (entity.WhitelistAgreements || []).forEach(({ permission, permitted }) => {
-    if (policies[permission]) policies[permission].allowlist.push(permitted);
-  });
-
-  return policies;
+      return {
+        ...acc,
+        [permId]: permPolicy
+      };
+    }, {});
 };
 
 export default {
   IDS,
   TYPES,
+  POLICY_IDS,
   POLICY_TYPES,
   MAX_POLICY_DURATION,
 
