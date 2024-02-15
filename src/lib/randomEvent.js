612a70232a8ac21124050ae6f4c737e45358ba98
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-02-15 14:43:47 -0500

    add random event titles

--- src/lib/randomEvent.js
+++ src/lib/randomEvent.js
@@ -1,5 +1,3 @@
-import procedural from '../utils/procedural.js';
-
 const IDS = {
   STARDUST: 1,
   GROUNDBREAKING: 2,
@@ -13,34 +11,42 @@ const IDS = {
 
 const TYPES = {
   [IDS.STARDUST]: {
+    name: 'Stardust',
     choices: [1],
-    maxChance: 0.75
+    maxChance: 0.75,
   },
   [IDS.GROUNDBREAKING]: {
+    name: 'Groundbreaking',
     choices: [1],
     maxChance: 0.75
   },
   [IDS.KEEP_EM_SEPARATED]: {
+    name: 'Keep \'Em Separated',
     choices: [1],
     maxChance: 0.75
   },
   [IDS.NO_SOUND_IN_SPACE]: {
+    name: 'No Sound in Space',
     choices: [1],
     maxChance: 0.75
   },
   [IDS.THE_CAKE_IS_A_HALF_TRUTH]: {
+    name: 'The Cake is a Half-Truth',
     choices: [1],
     maxChance: 0.75
   },
   [IDS.FLY_ME_TO_THE_MOON]: {
+    name: 'Fly Me to the Moon',
     choices: [1],
     maxChance: 0.75
   },
   [IDS.ALWAYS_LEAVE_A_NOTE]: {
+    name: 'Always Leave a Note',
     choices: [1],
     maxChance: 0.75
   },
   [IDS.GREATNESS]: {
+    name: 'Greatness',
     choices: [1],
     maxChance: 0.75
   }
